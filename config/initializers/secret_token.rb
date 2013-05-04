bb24431af40c4b4ba669e8f224dce1735a9085dc
josephooi <josephooijw@gmail.com>
2013-05-04 18:41:51 -0400

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pgcattendance::Application.config.secret_token = '011f285079df31765470dbdc46c79bc3db8aff63fe9ab228909abaa6a980ad8b3b68242a7e53ad22219c692941e4f6d1a5e60c9ec34a63e5d48d1da6abd005a9'
